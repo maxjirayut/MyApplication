33160ac81e502689ed7e192b3b542baa4abdacce
jirayut <jirayutmaxza@gmail.com>
2017-04-27 12:03:27 +0700

    config json and chenk User

--- app/src/main/java/com/example/ghkgkf/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ghkgkf/myapplication/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Explicit
@@ -77,6 +80,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void checkUserAnPass() {
         try {
+
+            String urlJSON = "http://swiftcodingthai.com/cph/getDataJirayut.php";
+            boolean b = true;
+            String[] columnStrings = new String[]{"id", "Name", "User", "Password"};
+            String[] loginStrings = new String[columnStrings.length];
+
+
+
+            GetData getData = new GetData(MainActivity.this);
+            getData.execute(urlJSON);
+            String strJSON = getData.get();
+            Log.d("27AprilV1", "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                if (userString.equals(jsonObject.getString(columnStrings[2]))) {
+                    b = false;
+                    for (int i1=0;i1<columnStrings.length;i1++) {
+                        loginStrings[i1] = jsonObject.getString(columnStrings[i1]);
+                        Log.d("27AprilV1", "loginString(" + i1 + ") ==> " + loginStrings[i1]);
+                    }
+
+                }
+            }
 
         } catch (Exception e) {
             Log.d("27AprilV1", "e checkUser ==> " + e.toString());
