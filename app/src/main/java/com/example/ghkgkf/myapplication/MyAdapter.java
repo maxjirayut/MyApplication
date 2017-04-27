1f679205579d343af023784e051623deee24f23f
jirayut <jirayutmaxza@gmail.com>
2017-04-27 16:39:19 +0700

    Finish 27April

---
+++ app/src/main/java/com/example/ghkgkf/myapplication/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.example.ghkgkf.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by ghkgkf on 27/4/2560.
+ */
+
+public class MyAdapter extends BaseAdapter{
+
+    private Context context;
+    private String[] nameStrings, dateStrings, detailStrings;
+
+    public MyAdapter(Context context,
+                     String[] nameStrings,
+                     String[] dateStrings,
+                     String[] detailStrings) {
+        this.context = context;
+        this.nameStrings = nameStrings;
+        this.dateStrings = dateStrings;
+        this.detailStrings = detailStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.my_listview, parent, false);
+
+        TextView nameTextView = (TextView) view.findViewById(R.id.txtName);
+        TextView dateTextView = (TextView) view.findViewById(R.id.tetDetail);
+        TextView detaTextView = (TextView) view.findViewById(R.id.tetDetail);
+
+        nameTextView.setText(nameStrings[position]);
+        dateTextView.setText(dateStrings[position]);
+        detaTextView.setText(detailStrings[position]);
+
+        return view;
+    }
+} //Main class
--- app/src/main/java/com/example/ghkgkf/myapplication/ServiceActivity.java
+++ app/src/main/java/com/example/ghkgkf/myapplication/ServiceActivity.java
@@ -2,12 +2,83 @@ package com.example.ghkgkf.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import static com.example.ghkgkf.myapplication.R.id.edtName;
+import static com.example.ghkgkf.myapplication.R.id.info;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
+    private ListView listView;
+    private String[] loginStrings, nameStrings, dateStrings, detaStrings, qrCodeStrings ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        initialView();
+
+        //Get Value From intent
+        getValueFromIntent();
+
+        createListView();
+
+    }// Main Method
+
+    private void createListView() {
+        String tag = "27AtrilV2";
+        String urlPHP = "http://swiftcodingthai.com/cph/getProduct.php";
+
+        try {
+
+            GetData getData = new GetData(ServiceActivity.this);
+            getData.equals(urlPHP);
+            String strJSON = getData.get();
+            Log.d(tag, "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            nameStrings = new String[jsonArray.length()];
+            dateStrings = new String[jsonArray.length()];
+            detaStrings = new String[jsonArray.length()];
+            qrCodeStrings = new String[jsonArray.length()];
+
+            for (int i=0; i<jsonArray.length();i++) {
+                JSONObject jsonObject  = jsonArray.getJSONObject(i);
+                nameStrings[i] = jsonObject.getString("name");
+                dateStrings[i] = jsonObject.getString("date_Receive");
+                detaStrings[i] = jsonObject.getString("Description");
+                qrCodeStrings[i] = jsonObject.getString("QR_code");
+
+            }
+
+
+
+        } catch (Exception e) {
+            Log.d(tag, "e createListView ==> " + e.toString());
+        }
+
     }
-}
+
+    private void getValueFromIntent() {
+        loginStrings = getIntent().getStringArrayExtra("Login");//ส่งค่ามาจากหน้าlogin
+        textView.setText(loginStrings[1]);
+    }
+
+    private void initialView() {
+        textView = (TextView) findViewById(R.id.txtName);
+        imageView = (ImageView) findViewById(R.id.imvQR);
+        listView = (ListView) findViewById(R.id.livProduct);
+
+    }
+}// Main class
