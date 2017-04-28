90d523c52b1d5ce665fa5e41c5cf7e92e429ed46
jirayut <jirayutmaxza@gmail.com>
2017-04-28 10:04:53 +0700

    subString Ok

--- app/src/main/java/com/example/ghkgkf/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/ghkgkf/myapplication/MyAdapter.java
@@ -53,8 +53,22 @@ public class MyAdapter extends BaseAdapter{
 
         nameTextView.setText(nameStrings[position]);
         dateTextView.setText(dateStrings[position]);
-        detaTextView.setText(detailStrings[position]);
+        detaTextView.setText(createDetailShow(detailStrings[position]));
 
         return view;
+    }
+
+    private String createDetailShow(String detailString) {
+
+        String result = null;
+
+        if (detailString.length() >= 30) {
+            result = detailString.substring(0, 30) + " ...";
+
+        } else {
+            result = detailString;
+        }
+
+        return result;
     }
 } //Main class
